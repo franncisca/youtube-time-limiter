19d8d6e08d308d00b10cac6265563e23fb8c77e6
Yan LI <yan.li@edu.univ-eiffel.fr>
2026-04-18 17:41:47 +0200

    feat: add options page and load content.js and option.js successfully

---
+++ content.js
@@ -0,0 +1,9 @@
+/**
+ * This script is injected into YouTube watch pages.
+ *
+ * When a user opens a YouTube video page,
+ * Chrome injects this script after the page is fully loaded
+ * (configured via manifest.json content_scripts).
+ */
+
+console.log("[YT LIMITER] hello from extension");
